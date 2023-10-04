ae3c07d53261af8a7141f12cd07bb0e85dceb954
Sallyzahran <sallyzahran.sz@gmail.com>
2023-10-04 11:05:23 +0300

    edit use class

--- functions.php
+++ functions.php
@@ -1,69 +1,79 @@
 <?php
 
+class CustomPostType{
+
+    public function __construct() {
+        add_action('init',array($this,'register_custom_post_type'));
+        add_action('init',array($this,'add_taxonomy'));
 
 
+    }
+    public function register_custom_post_type(){
 
-function custom_post_type(){
-
-$args = array(
-
-'labels' => array(
-'name'=>'Products',
-'signular_name' => 'Product',
-
-),
-
-
-'public' => true,
-'has_archive' => true,
-'supports'=>array('title','thumbnail','editor'),
-'menu_icon'=>'dashicons-products',
-'hierarchical'       => false,
-
-
-
-
-
-);
-
-register_post_type('products',$args);
-
-
-}
-
-
-add_action('init','custom_post_type');
-
-
-
-
-function add_taxonomy(){
-
-
-    $args = array(
-
+        $args = array(
+        
         'labels' => array(
-
-            'name' =>'Brands',
-         'signular_name' => 'Brand',
-            
+        'name'=>'Products',
+        'signular_name' => 'Product',
+        
         ),
-        'public'=>true,
-       'hierarchical' => true,
+        
+        
+        'public' => true,
+        'has_archive' => true,
+        'supports'=>array('title','thumbnail','editor'),
+        'menu_icon'=>'dashicons-products',
+        'hierarchical'   => false,
+        
+        
+        
+        
+        
+        );
+        
+        register_post_type('products',$args);
+        
+        
+        }
+        
+        
+
+       public function add_taxonomy(){
 
 
-    );
+            $args = array(
+        
+                'labels' => array(
+        
+                    'name' =>'Brands',
+                 'signular_name' => 'Brand',
+                    
+                ),
+                'public'=>true,
+               'hierarchical' => true,
+        
+        
+            );
+        
+            register_taxonomy('brands', array('products'), $args);
 
-
-    register_taxonomy('brands',array('products'),$args);
+        }
+        
 }
 
 
 
 
 
+$custom_post_type = new CustomPostType();
 
-add_action('init','add_taxonomy');
+
+
+
+
+
+
+
 
 
 ?>
