cb34ef27795020242ac9c0da596d8bb8cf69a65e
Sallyzahran <sallyzahran.sz@gmail.com>
2023-10-04 13:41:09 +0300

    add the editor metabox

--- functions.php
+++ functions.php
@@ -70,7 +70,31 @@ $custom_post_type = new CustomPostType();
 
 
 
+function add_editor_name_metabox(){
 
+
+
+    add_meta_box('editor-name-metabox','Editor Name','render_editor_name_metabox','post','side','high');
+}
+    add_action('add_meta_boxes','add_editor_name_metabox');
+
+    function render_editor_name_metabox($post){
+
+        $editor_name = get_post_meta($post->ID, 'editor_name', true);
+        ?>
+        <label for="editor-name">Editor Name:</label>
+        <input type="text" id="editor-name" name="editor_name" value="<?php echo esc_attr($editor_name); ?>">
+        <?php
+    }
+    
+    function save_editor_name($post_id) {
+        if (array_key_exists('editor_name', $_POST)) {
+            update_post_meta($post_id, 'editor_name', sanitize_text_field($_POST['editor_name']));
+    }
+}
+
+
+add_action('save_post','save_editor_name')
 
 
 
